33d7738c11da3c41fd56b39ebf0d84bda35e4b63
Hydroper <HydroperHH@gmail.com>
2020-10-13 17:13:56 -0300

    Commit

--- index.js
+++ index.js
@@ -45,6 +45,42 @@ export class StringExtension {
     }
 }
 
+export class LiveValue {
+    _value;
+    _listeners = [];
+
+    constructor(initialValue) {
+        this._value = initialValue;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    get() {
+        return this._value;
+    }
+
+    set(value) {
+        this._value = value;
+        for (let fn of this._listeners)
+            fn(this._value);
+    }
+
+    subscribe(fn) {
+        return this.setListener(fn);
+    }
+
+    setListener(fn) {
+        this._listeners.push(fn);
+        fn(this._value);
+        return () => {
+            let i = this._listeners.indexOf(fn);
+            if (i != -1) this._listeners.splice(i, 1);
+        };
+    }
+}
+
 /**
  * Creates an enum class like ShockScript enums.
  *
